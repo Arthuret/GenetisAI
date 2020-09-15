9ac26cb90b951190a66840fae66000ac5d154102
Arthur France <france.arthur@free.fr>
2020-09-15 08:20:09 +0200

    Update Variables.java
    Added description to variables

--- src/formula/Variables.java
+++ src/formula/Variables.java
@@ -1,11 +1,21 @@
 package formula;
 
 public enum Variables {
-	DISTANCE,
-	SPEED,
-	NB_STEPS,
-	WIN,
-	DEAD,
-	MAX_DISTANCE,
-	MAX_SPEED
+	DISTANCE("Distance to the objective (pixels)"),
+	SPEED("The speed of the dot at it's last move (just before it wins or die) (pixels/step)"),
+	NB_STEPS("The number of steps the dot played (before it wins or die)"),
+	WIN("1 if the dot reached the objective, 0 otherwise"),
+	DEAD("1 if the dot touched a wall, 0 otherwise"),
+	MAX_DISTANCE("The maximum distance possible on the current terrain (a diagonal)"),
+	MAX_SPEED("The speed limit of the dot (set in brain parameters)");
+	
+	private String description;
+	
+	Variables(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
 }
